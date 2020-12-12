eb5ab63a9de85737cca75da74c9efa04c3a81d8a
Abhishek Sehgal <sehgal.a.abhishek@gmail.com>
2020-12-11 19:31:16 -0600

    Memoized Construction of the String Construction

--- Dynamic_Programming/all_construct_dp.py
+++ Dynamic_Programming/all_construct_dp.py
@@ -1,25 +1,26 @@
 def all_construct(target, word_bank, memo=None):
-    if memo is None: memo = {}
-    if target in memo: return memo[target]
-    if target == '': return [[]]
+    if memo is None:
+        memo = {}
+    if target in memo:
+        return memo[target]
+    if target == "":
+        return [[]]
 
-    ways = []
+    result = []
 
     for word in word_bank:
         if target.startswith(word):
-            suffix = target[len(word):]
-            result = all_construct(suffix, word_bank, memo)
+            suffix = target[len(word) :]
+            suffix_ways = all_construct(suffix, word_bank, memo)
+            target_ways = list(map(lambda x: [word, *x], suffix_ways))
+            result.extend(target_ways)
 
-    return ways
+    # Comment this line for brute force
+    memo[target] = result
+    return result
 
 
 print(all_construct("purple", ["purp", "p", "ur", "le", "purpl"]))
-print(all_construct("abcdef", ["ab", "abc", "cd", "def", "abcd"]))
+print(all_construct("abcdef", ["ab", "abc", "cd", "def", "abcd", "ef", "c"]))
 print(all_construct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"]))
-print(all_construct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"]))
-print(
-    all_construct(
-        "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",
-        ["e", "ee", "eee", "eeee", "eeeee", "eeeeee"],
-    )
-)
+print(all_construct("aaaaaaaaaaaaaaaaaaaaaaaaaaz", ["a", "aa", "aaa", "aaaa", "aaaaa"])) # If we remove the z, it becomes the worst case scenario
